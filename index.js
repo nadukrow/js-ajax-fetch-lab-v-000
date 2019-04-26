3971412f1f712eabaea9b8adb465dc4d86114e37
Nana Adu-Krow <nadukrow@gmail.com>
2019-04-26 10:18:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
-function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return '';
+function getIssues() {
+  // GET /repos/:owner/:repo/issues
+  const repo = 'ackerm44/javascript-fetch-lab'
+
+  fetch (`https://api.github.com/repos/${repo}/issues`, {
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(res => res.json().then(json => showIssues(json)))
+}
+// `${response.items.map(r => displaySearchResult(r)).join('')}`
+
+function renderIssues(j) {
+  return `<div>
+            <h4>${j.title}</h4>
+            <p><em>${j.user.login}</em></p>
+            <p>${j.body}</p>
+          </div>`
 }
 
-function forkRepo() {
-  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
-  //use fetch to fork it!
-}
 
-function showResults(json) {
-  //use this function to display the results from forking via the API
+function showIssues(json) {
+  const issues = `<ul>${json.map(j => renderIssues(j)).join('')}`
+  $('#issues').html(issues)
 }
 
 function createIssue() {
-  //use this function to create an issue based on the values input in index.html
+  const repo = 'ackerm44/javascript-fetch-lab'
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  }
+  fetch (`https://api.github.com/repos/${repo}/issues`, {
+    method: 'post',
+    body: JSON.stringify(postData),
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(getIssues())
 }
 
-function getIssues() {
-  //once an issue is submitted, fetch all open issues to see the issues you are creating
+function showResults(json) {
+}
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  //use fetch to fork it!
+  // POST /repos/:owner/:repo/forks
+  fetch (`https://api.github.com/repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    }
+  }).then(res => res.json().then(json => showForkedRepo(json)))
+}
+
+
+function showForkedRepo(json) {
+  const repo = `<a href='${json.html_url}'>Link To Fork</a>`
+  $("#results").html(repo)
+}
+
+function getToken() {
+  //change to your token to run in browser, but set
+  //back to '' before committing so all tests pass
+  return ''
 }
