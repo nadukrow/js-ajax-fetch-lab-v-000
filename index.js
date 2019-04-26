6b2c64ce1396e5468651000d517bfc7d833b0d9a
Nana Adu-Krow <nadukrow@gmail.com>
2019-04-26 10:20:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,67 +1,65 @@
-function getIssues() {
-  const repo = 'Adjoa/javascript-fetch-lab'
-  
-  // GET /repos/:owner/:repo/issues
-  fetch(`https://api.github.com/${repo}/issues`, {
-    headers: {
-      Authorization: `token ${getToken()}`
-    }
-  }).then(res => res.json()).then(json => showIssues(json));
-}
-
-
-function showIssues(json) {
-   const issues_html = `
-    <ul>
-      ${json.map(issue => `
-      <li>
-        <p>${issue.title}</p>
-        <p>${issue.body}</p>
-        <p>${issue.user.login}</p>
-      </li>`).join('')}
-    </ul>`
-  
-  $('#issues').html(issues_html)
-}
-
-
-function createIssue() {
-  const repo = 'Adjoa/javascript-fetch-lab'
-  const postData = {
-    "title": `${document.getElementById('title').value}`,
-    "body": `${document.getElementById('body').value}`,
-  }
-
-  // POST /repos/:owner/:repo/issues
-  fetch(`https://api.github.com/${repo}/issues`, {
-    method: 'post',
-    body: JSON.stringify(postData),
-    headers: {
-      Authorization: `token ${getToken()}`
-    }
-  }).then(getIssues());
-}
-
-
-function showResults(json) {
-  html_string= `<a href=${json.html_url}>${json.name}</a>`
-  $('#results').html(html_string)
-}
-
-function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
-  // POST /repos/:owner/:repo/forks
-  fetch(`https://api.github.com/repos/${repo}/forks`, {
-    method: 'post',
-    headers: {
-      Authorization: `token ${getToken()}`
-    }
-  }).then(res => res.json()).then(json => showResults(json));
-}
+//const token = ''
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '';
 }
+
+function forkRepo() {
+  const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  //use fetch to fork it!
+  fetch('https://api.github.com/repos/'+ repo +'/forks',
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`
+      }
+    })
+   .then(res => res.json())
+   .then(json => showResults(json));
+}
+
+function showResults(json) {
+  //use this function to display the results from forking via the API
+  $('#results').append(`<a href=${json.html_url}>${json.html_url} </a>` )
+}
+
+function createIssue() {
+  //use this function to create an issue based on the values input in index.html
+  const repo = `81Jeremiah/js-ajax-fetch-lab`;
+  //const url = 'https://api.github.com/repos/'+ repo +'/issues';
+  const data = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  };
+
+  fetch('https://api.github.com/repos/'+ repo +'/issues', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
+    .then(res => res.json())
+    .then(json => getIssues());
+}
+
+function getIssues() {
+  //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = `81Jeremiah/js-ajax-fetch-lab`;
+  fetch('https://api.github.com/repos/'+ repo +'/issues', {
+    headers: {
+     Authorization: `token ${getToken()}`
+   }
+ })
+   .then(res => res.json())
+   .then(json => showIssues(json));
+}
+
+function showIssues(json){
+  $('#issues').append( `${json}`)
+}
+// function displayIssues(json)
+//     for(body in json) {`${body}`)//`Title:${json.title}`
+// }
